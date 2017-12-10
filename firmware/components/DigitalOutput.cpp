f45717675b3c999f02ce3121cf1ff6f3cb906da0
Kier Davis <kierdavis@gmail.com>
2017-12-10 21:33:41 +0000

    firmware/DigitalOutput: add operations for initialising/settting whole ports at a time

--- firmware/components/DigitalOutput.cpp
+++ firmware/components/DigitalOutput.cpp
@@ -1,10 +1,11 @@
 #include <stdbool.h>
+#include <stdint.h>
 #include <avr/io.h>
 
 #include "DigitalOutput.hpp"
 #include "Port.hpp"
 
-void DigitalOutput::init(const Pin &pin) {
+void DigitalOutput::initPin(const Pin &pin) {
   #ifdef __AVR_ATmega644P__
     *pin.port.DDR |= _BV(pin.index);
   #else
@@ -12,13 +13,29 @@ void DigitalOutput::init(const Pin &pin) {
   #endif
 }
 
-void DigitalOutput::set(const Pin &pin, bool state) {
+void DigitalOutput::setPin(const Pin &pin, bool state) {
   #ifdef __AVR_ATmega644P__
     if (state) {
       *pin.port.PORT |= _BV(pin.index);
     } else {
       *pin.port.PORT &= ~_BV(pin.index);
     }
+  #else
+    #error "unsupported target device"
+  #endif
+}
+
+void DigitalOutput::initPort(const Port &port) {
+  #ifdef __AVR_ATmega644P__
+    *port.DDR = 0xFF;
+  #else
+    #error "unsupported target device"
+  #endif
+}
+
+void DigitalOutput::setPort(const Port &port, uint8_t state) {
+  #ifdef __AVR_ATmega644P__
+    *port.PORT = state;
   #else
     #error "unsupported target device"
   #endif
--- firmware/components/DigitalOutput.hpp
+++ firmware/components/DigitalOutput.hpp
@@ -5,15 +5,22 @@
 #define __TPC_DIGITALOUTPUT_HPP
 
 #include <stdbool.h>
+#include <stdint.h>
 
 #include "Port.hpp"
 
 namespace DigitalOutput {
   // Initialise a pin as a digital output.
-  void init(const Pin &pin);
+  void initPin(const Pin &pin);
 
   // Set the state of a digital output (high or low).
-  void set(const Pin &pin, bool state);
+  void setPin(const Pin &pin, bool state);
+
+  // Initialise all pins on a port as digital outputs.
+  void initPort(const Port &port);
+
+  // Set the state of all pins on a port at once.
+  void setPort(const Port &port, uint8_t state);
 }
 
 #endif // #ifndef __TPC_DIGITALOUTPUT_HPP
