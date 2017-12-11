42beb321d6283f44200911478cb8034e04b78b47
Kier Davis <kierdavis@gmail.com>
2017-12-11 15:23:32 +0000

    firmware/DigitalOutput: refactor

--- firmware/modules/DigitalOutput.cpp
+++ firmware/modules/DigitalOutput.cpp
@@ -5,38 +5,25 @@
 #include "DigitalOutput.hpp"
 #include "Port.hpp"
 
-void DigitalOutput::initPin(const Pin &pin) {
-  #ifdef __AVR_ATmega644P__
-    *pin.port.DDR |= _BV(pin.index);
-  #else
-    #error "unsupported target device"
-  #endif
-}
+#ifdef __AVR_ATmega644P__
 
-void DigitalOutput::setPin(const Pin &pin, bool state) {
-  #ifdef __AVR_ATmega644P__
+  void DigitalOutput::initPin(const Pin &pin) {
+    *pin.port.DDR |= _BV(pin.index);
+  }
+  void DigitalOutput::setPin(const Pin &pin, bool state) {
     if (state) {
       *pin.port.PORT |= _BV(pin.index);
     } else {
       *pin.port.PORT &= ~_BV(pin.index);
     }
-  #else
-    #error "unsupported target device"
-  #endif
-}
-
-void DigitalOutput::initPort(const Port &port) {
-  #ifdef __AVR_ATmega644P__
+  }
+  void DigitalOutput::initPort(const Port &port) {
     *port.DDR = 0xFF;
-  #else
-    #error "unsupported target device"
-  #endif
-}
-
-void DigitalOutput::setPort(const Port &port, uint8_t state) {
-  #ifdef __AVR_ATmega644P__
+  }
+  void DigitalOutput::setPort(const Port &port, uint8_t state) {
     *port.PORT = state;
-  #else
-    #error "unsupported target device"
-  #endif
-}
+  }
+
+#else
+  #error "unsupported target device"
+#endif
