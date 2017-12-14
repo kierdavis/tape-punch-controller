1ebabd4648c2ffbbf7facfaaac2a4d9e7de51af0
Kier Davis <kierdavis@gmail.com>
2017-12-14 22:47:56 +0000

    firmware: aaaaaaaaaaaaaaaaaaaa
    Aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa.

---
+++ firmware/compile_everything.sh
@@ -0,0 +1,64 @@
+#!/bin/sh
+
+set -o errexit  # Exit script if any command fails...
+set -o pipefail # ...include commands within a pipeline.
+set -o nounset  # Exit script if an undefined variable is used.
+
+self="$(realpath $0)"
+firmware_dir="$(dirname $self)"
+modules_dir="$firmware_dir/modules"
+build_dir="$firmware_dir/build"
+
+device="${DEVICE:-}"
+if [ -z "$device" ]; then
+  device="atmega644p"
+  echo "Note: using the default value of DEVICE, which is $device."
+  echo "If you want to change this (or squelch this warning), set the DEVICE environment variable."
+  echo
+fi
+
+f_cpu="${F_CPU:-}"
+if [ -z "$f_cpu" ]; then
+  f_cpu="12000000"
+  echo "Note: using the default value of F_CPU (CPU frequency in Hz), which is $f_cpu."
+  echo "If you want to change this (or squelch this warning), set the F_CPU environment variable."
+  echo
+fi
+
+device_modules_dir="$modules_dir/device-specific/$device"
+
+include_path="-I$modules_dir -I$device_modules_dir"
+
+cflags="-Wall"                     # Enable all warnings
+cflags="$cflags -std=c++11"        # Specify language version
+cflags="$cflags -O2"               # Enable standard optimisations
+cflags="$cflags -flto"             # Enable link-time- (aka whole-program-) optimisation
+cflags="$cflags -mmcu=$device"     # Inform the compiler of the target device
+cflags="$cflags -DF_CPU=$f_cpu"    # Inform library code of the CPU frequency
+cflags="$cflags $include_path"     # Add -I flags
+cflags="$cflags ${CFLAGS:-}"       # Append user's CFLAGS variable if present
+
+ldflags="-Wall"                    # Enable all warnings
+ldflags="$ldflags -O2"             # Enable optimisations
+ldflags="$ldflags -mmcu=$device"   # Inform the compiler of the target device
+ldflags="$ldflags ${LDFLAGS:-}"    # Append user's LDFLAGS variable if present
+
+# Create a directory for build files / empty it if it already exists.
+mkdir -p "$build_dir"
+rm -rf $build_dir/*
+
+# Compile source files.
+sources="$firmware_dir/main.cpp $(find $modules_dir $device_modules_dir -maxdepth 1 -name '*.cpp')"
+echo "Compiling everything at once"
+cat $sources > "$build_dir/everything.cpp"
+elf_file="$build_dir/firmware.elf"
+avr-g++ $cflags $ldflags -o "$elf_file" "$build_dir/everything.cpp"
+
+# Convert to hex.
+hex_file="$build_dir/firmware.hex"
+echo "Converting $(realpath --relative-base=$PWD $elf_file)"
+echo "        to $(realpath --relative-base=$PWD $hex_file)"
+echo
+avr-objcopy -O ihex "$elf_file" "$hex_file"
+
+echo "Done."
--- firmware/main.cpp
+++ firmware/main.cpp
@@ -1,15 +1,161 @@
 #include <stdint.h>
 
 #include <avr/interrupt.h>
+#include <avr/pgmspace.h>
+#include <util/delay.h>
 
 #include "FeedbackSignal.hpp"
 #include "Hooks.hpp"
 #include "Solenoids.hpp"
 #include "SolenoidTimer.hpp"
 
-void Hooks::onFeedbackPulse() {
-  Solenoids::pulse(0xFF);
+// static uint8_t body = 200;
+// static uint8_t pos = 0;
+// static const uint8_t sequence[] PROGMEM = {
+//   0b00001,
+//   0b00010,
+//   0b00100,
+//   0b01000,
+//   0b10000,
+//   0b01000,
+//   0b00100,
+//   0b00010,
+// };
+// static bool moreDataBytes() {
+//   return body > 0;
+// }
+// static uint8_t nextDataByte() {
+//   uint8_t value = 0;
+//   if (body > 0) {
+//     value = pgm_read_byte(&sequence[pos]);
+//     pos = (pos + 1) % 8;
+//     body--;
+//   }
+//   return value;
+// }
+
+static uint8_t pos = 0;
+static const uint8_t data[] PROGMEM = {
+  0x1f,
+  0x02,
+  0x04,
+  0x02,
+  0x1f,
+  0x00,
+  0x00,
+  0x1f,
+  0x15,
+  0x11,
+  0x00,
+  0x00,
+  0x1f,
+  0x05,
+  0x1a,
+  0x00,
+  0x00,
+  0x1f,
+  0x05,
+  0x1a,
+  0x00,
+  0x00,
+  0x03,
+  0x1c,
+  0x03,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x00,
+  0x11,
+  0x0a,
+  0x04,
+  0x0a,
+  0x11,
+  0x00,
+  0x00,
+  0x1f,
+  0x02,
+  0x04,
+  0x02,
+  0x1f,
+  0x00,
+  0x00,
+  0x1e,
+  0x05,
+  0x1e,
+  0x00,
+  0x00,
+  0x12,
+  0x15,
+  0x09,
+  0x00,
+  0x00,
+};
+static bool moreDataBytes() {
+  return pos < (sizeof(data)/sizeof(data[0]));
 }
+static uint8_t nextDataByte() {
+  return pgm_read_byte(&data[pos++]);
+}
+
+static uint8_t wait = 200;
+static uint8_t lead_in = 0;
+static uint8_t lead_out = 70;
+static uint8_t skip = 0;
+
+void Hooks::onFeedbackPulse() {
+  skip = (skip + 1) % 2;
+  if (skip < 1) return;
+  _delay_us(250);
+  uint8_t value = 0x00;
+  if (wait > 0) {
+    wait--;
+    return;
+  }
+  else if (lead_in > 0) {
+    lead_in--;
+  }
+  else if (moreDataBytes()) {
+    value = nextDataByte();
+  }
+  else if (lead_out > 0) {
+    lead_out--;
+  }
+  else {
+    return;
+  }
+  Solenoids::pulse(value);
+}
+
+// static uint8_t wait = 200;
+// static uint8_t lead_in = 15;
+// static uint8_t lead_out = 50;
+// static uint8_t pos = 0;
+
+
+// void Hooks::onFeedbackPulse() {
+//   uint8_t value = 0x00;
+//   if (wait > 0) {
+//     wait--;
+//     return;
+//   }
+//   else if (lead_in > 0) {
+//     lead_in--;
+//   }
+//   else if (pos < sizeof(data)/sizeof(data[0])) {
+//     value = pgm_read_byte(&data[pos]);
+//     pos++;
+//   }
+//   else if (lead_out > 0) {
+//     lead_out--;
+//   }
+//   else {
+//     return;
+//   }
+//   Solenoids::pulse(value);
+// }
 
 int main() {
   Solenoids::init();
