377a427ceaa82b1bbc02bd5284a294db6b4206e6
Kier Davis <kierdavis@gmail.com>
2018-01-09 15:56:12 +0000

    Update feed solenoid pin
    The description of the setup in my logbook uses says pin PB0, not PB1.

--- firmware/modules/device-specific/atmega644p/Config.cpp
+++ firmware/modules/device-specific/atmega644p/Config.cpp
@@ -3,7 +3,7 @@
 
 const IO::Port &Config::codeSolenoidPort = IO::Port::A;
 const IO::Pin Config::feedSolenoidPin =
-  { .port = IO::Port::B, .index = 1 };
+  { .port = IO::Port::B, .index = 0 };
 const IO::ExternalInterruptPin Config::feedbackPin =
   IO::ExternalInterruptPin::INT2_;
 
