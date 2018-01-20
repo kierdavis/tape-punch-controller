ba08c8ee71c8128d87120a1cd6b6771ed228480b
Kier Davis <kierdavis@gmail.com>
2018-01-20 18:32:28 +0000

    firmware: use correct naming convention for constants

--- firmware/src/Config.hpp
+++ firmware/src/Config.hpp
@@ -2,14 +2,14 @@
 #include "IO/Port.hpp"
 
 namespace Config {
-  static const IO::Port codeSolenoidPort =
+  static const IO::Port CODE_SOLENOID_PORT =
   #if PLATFORM == DEV_IL_MATTO
     IO::Port::A;
   #else
     #error "invalid or unsupported PLATFORM"
   #endif
 
-  static const IO::Pin feedSolenoidPin =
+  static const IO::Pin FEED_SOLENOID_PIN =
   #if PLATFORM == DEV_IL_MATTO
     { .port = IO::Port::B, .index = 0 };
   #else
