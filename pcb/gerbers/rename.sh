bd6ff475d239ae36b01e1db719d6aa57ec64c5de
Kier Davis <kierdavis@gmail.com>
2018-02-01 11:26:41 +0000

    pcb: add script to rename gerbers
    Kicad's default filenames cause the university email spam filter
    to flag the files as malware.

---
+++ pcb/gerbers/rename.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+mv *.gtl tape-punch-controller-top-copper.gtl
+mv *.gts tape-punch-controller-top-soldermask.gts
+mv *.gto tape-punch-controller-top-silk.gto
+mv *.gbl tape-punch-controller-bottom-copper.gbl
+mv *.gbs tape-punch-controller-bottom-soldermask.gbs
+mv *.gbo tape-punch-controller-bottom-silk.gbo
+mv *.gm1 tape-punch-controller-outline.gm1
+mv *.drl tape-punch-controller-drill.drl
