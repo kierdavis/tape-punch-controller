e712e640a072b5e8922198d5f6a987241750f27c
Kier Davis <kierdavis@gmail.com>
2018-02-19 21:35:36 +0000

    firmware: remove archived source code of earlier iterations
    It's no longer needed.

--- firmware/legacy/modules.old/FeedbackSignal.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __FEEDBACKSIGNAL_HPP
-#define __FEEDBACKSIGNAL_HPP
-
-namespace FeedbackSignal {
-  void init();
-}
-
-#endif // #ifndef __FEEDBACKSIGNAL_HPP
--- firmware/legacy/modules.old/device-specific/atmega644p/IO.cpp
+++
@@ -1,8 +0,0 @@
-#include <avr/io.h>
-
-#include "IO.hpp"
-
-const IO::Port IO::Port::A = { .DDR = &DDRA, .PORT = &PORTA, .PIN = &PINA };
-const IO::Port IO::Port::B = { .DDR = &DDRB, .PORT = &PORTB, .PIN = &PINB };
-const IO::Port IO::Port::C = { .DDR = &DDRC, .PORT = &PORTC, .PIN = &PINC };
-const IO::Port IO::Port::D = { .DDR = &DDRD, .PORT = &PORTD, .PIN = &PIND };
