f06f7c63cc4c6f99edad536c58200d86416355cc
Kier Davis <kierdavis@gmail.com>
2018-02-20 15:32:08 +0000

    firmware: clean up unused code in main.cpp

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -8,18 +8,6 @@
 #include "TPC/Application.hpp"
 #include "TPC/USBDriver.hpp"
 
-static uint8_t const DATA[] = {
-  0b10000,
-  0b01000,
-  0b00100,
-  0b00010,
-  0b00001,
-  0b00010,
-  0b00100,
-  0b01000,
-  0b10000,
-};
-
 int main() {
   // First stage init.
   TPC::MicrocontrollerDriver::init();
@@ -32,10 +20,6 @@ int main() {
   TPC::Application::init();
   sei();
   LOG("[main] ready");
-
-  // Controller::TapePunch::addJob_IE(100); // leader (100 blank rows)
-  // Controller::TapePunch::addJob_IE(sizeof(DATA)/sizeof(DATA[0]), DATA);
-  // Controller::TapePunch::addJob_IE(200); // trailer (200 blank rows)
 
   while (1) {
     TPC::TPController::tick_IE();
