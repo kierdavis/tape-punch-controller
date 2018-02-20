8b703ffced25ece69520d1fb1bc410b0c5e364d1
Kier Davis <kierdavis@gmail.com>
2018-02-20 09:59:22 +0000

    firmware: remove now-redundant Controller::JobQueue module and co.

--- firmware/src/Controller/Job.cpp
+++
@@ -1,9 +0,0 @@
-#include <stdint.h>
-
-#include "Controller/Job.hpp"
-
-Controller::Job::Job(uint16_t length, const uint8_t * dataBuffer)
-  : length(length),
-    dataBuffer(dataBuffer),
-    dataPtr(dataBuffer),
-    next(nullptr) {}
