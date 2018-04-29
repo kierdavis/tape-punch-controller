04528b0dac757b9e5740ac6323ce553b23ad8d4e
Kier Davis <kierdavis@gmail.com>
2018-04-29 15:57:03 +0100

    firmware: reduce dependency on pdi-programmer
    Technically, any PDI programmer can be used, not just this specific one.
    For convienence, the Nix shell file will still use this one, but the
    expression for it is fetched only when evaluated, rather than being
    downloaded as a Git submodule.
    
    (cherry picked from commit addd4707eee5a444b0a82f3215493288713fa190)

--- firmware/shell.nix
+++ firmware/shell.nix
@@ -3,7 +3,8 @@
 with pkgs;
 
 let
-  pdiprog = pkgs.callPackage ./pdi-programmer/client {};
+  pdiprog-src = fetchTarball https://github.com/kierdavis/pdi-programmer/archive/1db20221af78bfecaf71d872678ff31be1b0fd5e.tar.gz;
+  pdiprog = pkgs.callPackage (import "${pdiprog-src}/client") {};
 
 in stdenv.mkDerivation {
   name = "tpc-firmware-env";
